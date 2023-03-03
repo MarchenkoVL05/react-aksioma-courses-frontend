f33a18ad68c52d58922b04874ec4cd30d3b9e5db
HP 6 <Shamoointhedark@gmail.com>
2023-03-03 11:56:59 +0500

    Исправлен баг с отображением видео в fullscreen

--- src/components/Video.js
+++ src/components/Video.js
@@ -14,25 +14,38 @@ function Video({ lesson }) {
         {
           src: `http://localhost:4444${lesson.videoUrl}`,
           type: "video/mp4",
-          size: 1080,
         },
       ],
     },
     options: {
-      settings: ["captions", "quality", "speed", "loop"],
+      controls: [
+        "play-large",
+        "play",
+        "progress",
+        "current-time",
+        "mute",
+        "volume",
+        "captions",
+        "settings",
+        "pip",
+        "airplay",
+        "fullscreen",
+      ],
       disableContextMenu: true,
-      quality: { default: 576, options: [1080, 720, 576, 480, 360, 240] },
-    },
-    style: {
-      width: "100%",
-      height: "400px",
+      fullscreen: {
+        enabled: true,
+        fallback: true,
+        iosNative: true,
+      },
     },
   };
 
   return (
     <>
       <div className="detail-video">
-        <Plyr {...plyrProps} />
+        <div className="detail-video__container">
+          <Plyr {...plyrProps} />
+        </div>
         <div className="detail-video__content">
           <h1 className="detail-video__title">{lesson.title}</h1>
           <p className="detail-video__descr">{lesson.content}</p>
