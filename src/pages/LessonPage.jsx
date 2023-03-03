2d5116ff2da4a6120d2fd5646765fa23fb6baa42
HP 6 <Shamoointhedark@gmail.com>
2023-03-03 13:29:09 +0500

    Поправлены стили warning'ов, переименован test.js

--- src/pages/LessonPage.jsx
+++ src/pages/LessonPage.jsx
@@ -1,15 +1,15 @@
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
 
-import useAuth from '../hooks/useAuth.js';
-import { fetchOneLesson } from '../redux/slices/lessonSlice.js';
+import useAuth from "../hooks/useAuth.js";
+import { fetchOneLesson } from "../redux/slices/lessonSlice.js";
 
-import Header from '../components/Header';
-import Video from '../components/Video.js';
-import Test from '../components/Test.jsx';
-import Loader from '../components/Loader';
-import ErrorPage from '../pages/ErrorPage';
+import Header from "../components/Header";
+import Video from "../components/Video";
+import Test from "../components/Test";
+import Loader from "../components/Loader";
+import ErrorPage from "../pages/ErrorPage";
 
 function LessonPage() {
   const dispatch = useDispatch();
@@ -26,15 +26,15 @@ function LessonPage() {
 
   return (
     <>
-      {lessonStatus === 'loading' && !error && <Loader />}
-      {lessonStatus !== 'loading' && !error && lesson && (
+      {lessonStatus === "loading" && !error && <Loader />}
+      {lessonStatus !== "loading" && !error && lesson && (
         <div>
           <Header userInfo={userInfo} />
-          <div className='detail-page-wrapper'>
-            <div className='detail-page'>
+          <div className="detail-page-wrapper">
+            <div className="detail-page">
               <Video lesson={lesson} />
             </div>
-            <div className='test'>
+            <div className="test">
               <Test lesson={lesson} />
             </div>
           </div>
