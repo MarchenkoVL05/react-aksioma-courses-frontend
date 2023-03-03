ebb24bbf61bd76bd866281269eb361d39f2464da
HP 6 <Shamoointhedark@gmail.com>
2023-03-03 10:53:39 +0500

    Plyr Video Player вместо обычного тега video

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,38 +1,44 @@
-import React, { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
+
+import Plyr from "plyr-react";
+import "plyr-react/plyr.css";
 
 function Video({ lesson }) {
   const navigate = useNavigate();
-  const [playbackRate, setPlaybackRate] = useState(1);
-  const videoRef = useRef(null);
 
-  const handleRateChange = (e) => {
-    setPlaybackRate(e.target.value);
-    videoRef.current.playbackRate = e.target.value;
+  const plyrProps = {
+    source: {
+      type: "video",
+      title: "video",
+      sources: [
+        {
+          src: `http://localhost:4444${lesson.videoUrl}`,
+          type: "video/mp4",
+          size: 1080,
+        },
+      ],
+    },
+    options: {
+      settings: ["captions", "quality", "speed", "loop"],
+      disableContextMenu: true,
+      quality: { default: 576, options: [1080, 720, 576, 480, 360, 240] },
+    },
+    style: {
+      width: "100%",
+      height: "400px",
+    },
   };
 
   return (
     <>
       <div className="detail-video">
-        <video controls="controls" controlsList="nodownload" ref={videoRef}>
-          <source src={`http://localhost:4444${lesson.videoUrl}`} type="video/ogg; codecs=theora, vorbis"></source>
-          <source
-            src={`http://localhost:4444${lesson.videoUrl}`}
-            type="video/mp4; codecs=avc1.42E01E, mp4a.40.2"
-          ></source>
-          <source src={`http://localhost:4444${lesson.videoUrl}`} type="video/webm; codecs=vp8, vorbis"></source>
-          Тег video не поддерживается вашим браузером.
-        </video>
+        <Plyr {...plyrProps} />
         <div className="detail-video__content">
           <h1 className="detail-video__title">{lesson.title}</h1>
           <p className="detail-video__descr">{lesson.content}</p>
           <button onClick={() => navigate("/")} className="detail-video__back">
             Вернуться к списку уроков
           </button>
-          <div className="detail-video__speed">
-            <label htmlFor="rate">Скорость:</label>
-            <input type="range" min="0.5" max="2" step="0.1" value={playbackRate} onChange={handleRateChange} />
-          </div>
         </div>
       </div>
     </>
