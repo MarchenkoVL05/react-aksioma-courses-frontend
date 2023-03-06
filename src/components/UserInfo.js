8b9cf2ba615dd8d4bddbdc85919e940dac648634
HP 6 <Shamoointhedark@gmail.com>
2023-03-06 18:09:49 +0500

    редизайн окна с информацией о профиле

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,4 +1,4 @@
-import logo from "../images/logo.svg";
+import logo from "../images/logo-white.svg";
 
 function UserInfo({ userInfo, open, toggleModal, setOpen }) {
   const logOut = () => {
@@ -12,13 +12,13 @@ function UserInfo({ userInfo, open, toggleModal, setOpen }) {
       {open && <div onClick={toggleModal} className="overlay"></div>}
       {open && (
         <div className="userInfo">
-          <img src={logo} alt="" />
-          <div className="userInfo-name">Полное имя: {userInfo.fullName}</div>
-          <div className="userInfo-workPosition">Отдел: {userInfo.workPosition}</div>
-          <div className="userInfo-role">
-            Роль пользователя: {userInfo.role == "admin" ? "Администратор" : "Ученик"}
+          <div className="userInfo__bg">
+            <img src={logo} alt="" />
+            <div className="userInfo__name">{userInfo.fullName}</div>
+            <div className="userInfo__workPosition">{userInfo.workPosition}</div>
           </div>
-          <div className="userInfo-approved">
+          <div className="userInfo__role">{userInfo.role == "admin" ? "Администратор" : "Ученик"}</div>
+          <div className="userInfo__approved">
             {userInfo.approved == true ? "Допусчен до курсов" : "Не допусчен до курсов"}
           </div>
           <button onClick={logOut} className="userInfo-logOut">
