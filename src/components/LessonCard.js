60f8724ecac2c5e45cf8233cb6b90a3c28928415
HP 6 <Shamoointhedark@gmail.com>
2023-03-06 16:25:52 +0500

    Вынесена карточка урока в отдельный компонент

---
+++ src/components/LessonCard.js
@@ -0,0 +1,39 @@
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { removeLesson } from "../redux/slices/lessonSlice";
+
+import preview from "../images/video-preview.png";
+import remove from "../images/removeLesson.svg";
+
+function LessonCard({ userInfo, lesson, setIsRemoveClicked }) {
+  const dispatch = useDispatch();
+
+  const handleRemoveLesson = (lessonId) => {
+    let confirmRemoving = window.confirm("Вы уверены что хотите удалить этот урок?");
+    if (confirmRemoving) {
+      dispatch(removeLesson(lessonId));
+      setIsRemoveClicked(true);
+    } else {
+      return;
+    }
+  };
+
+  return (
+    <div className="lessons__item" key={lesson._id}>
+      {userInfo.role == "admin" && (
+        <button onClick={() => handleRemoveLesson(lesson._id)} className="lessons__item-remove" type="button">
+          <img src={remove} />
+        </button>
+      )}
+      <Link to={`/lesson/${lesson._id}`} className="lessons__item-link-wrapper">
+        <img className="lessons__item-preview" src={preview} alt="" />
+        <div className="lessons__item-title">{lesson.title}</div>
+        <div className="lessons__item-content">
+          {lesson.content.length > 70 ? lesson.content.slice(0, 70) + "..." : lesson.content}
+        </div>
+      </Link>
+    </div>
+  );
+}
+
+export default LessonCard;
--- src/components/LessonsList.js
+++ src/components/LessonsList.js
@@ -1,32 +1,16 @@
 import { useRef, useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
+import LessonCard from "./LessonCard";
 import LessonLoader from "../components/LessonLoader";
-
-import { removeLesson } from "../redux/slices/lessonSlice";
-
-import preview from "../images/video-preview.png";
-import remove from "../images/removeLesson.svg";
 
 function LessonsList({ lessons, status, userInfo, searchError, searchStatus }) {
   const [isRemoveClicked, setIsRemoveClicked] = useState(false);
 
-  const dispatch = useDispatch();
   const warningRef = useRef();
 
   const removeError = useSelector((state) => state.lesson.error);
   const message = useSelector((state) => state.lesson.message);
-
-  const handleRemoveLesson = (lessonId) => {
-    let confirmRemoving = window.confirm("Вы уверены что хотите удалить этот урок?");
-    if (confirmRemoving) {
-      dispatch(removeLesson(lessonId));
-      setIsRemoveClicked(true);
-    } else {
-      return;
-    }
-  };
 
   useEffect(() => {
     if (removeError && isRemoveClicked) {
@@ -67,26 +51,7 @@ function LessonsList({ lessons, status, userInfo, searchError, searchStatus }) {
                     return <LessonLoader key={index} />;
                   })
                 : lessons.map((lesson) => {
-                    return (
-                      <div className="lessons__item" key={lesson._id}>
-                        {userInfo.role == "admin" && (
-                          <button
-                            onClick={() => handleRemoveLesson(lesson._id)}
-                            className="lessons__item-remove"
-                            type="button"
-                          >
-                            <img src={remove} />
-                          </button>
-                        )}
-                        <Link to={`/lesson/${lesson._id}`} className="lessons__item-link-wrapper" state={status}>
-                          <img className="lessons__item-preview" src={preview} alt="" />
-                          <div className="lessons__item-title">{lesson.title}</div>
-                          <div className="lessons__item-content">
-                            {lesson.content.length > 70 ? lesson.content.slice(0, 70) + "..." : lesson.content}
-                          </div>
-                        </Link>
-                      </div>
-                    );
+                    return <LessonCard userInfo={userInfo} lesson={lesson} setIsRemoveClicked={setIsRemoveClicked} />;
                   })}
             </div>
           </div>
