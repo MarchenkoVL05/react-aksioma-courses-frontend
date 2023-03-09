a6f4aab8fd647a4408c44e257eca48c9a9f48bc7
Vladimir <BITcheat@mail.ru>
2023-03-09 23:20:38 +0500

    Логика добавления вопроса к уроку

--- src/components/CreateQuestion.jsx
+++ src/components/CreateQuestion.jsx
@@ -1,15 +1,18 @@
-import React, { useState } from "react";
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { addQuestion } from '../redux/slices/lessonSlice';
 
 function CreateQuestion({ setCreate, lesson }) {
-  const [question, setQuestion] = useState("");
-  const [inputType, setInputType] = useState("radio");
+  const [question, setQuestion] = useState('');
+  const [inputType, setInputType] = useState('radio');
   const [options, setOptions] = useState([
-    { optionTitle: "", right: false },
-    { optionTitle: "", right: false },
+    { optionTitle: '', right: false },
+    { optionTitle: '', right: false },
   ]);
 
   const addOption = () => {
-    setOptions([...options, { optionTitle: "", right: false }]);
+    setOptions([...options, { optionTitle: '', right: false }]);
   };
 
   const handleOptionChange = (index, key, value) => {
@@ -17,6 +20,8 @@ function CreateQuestion({ setCreate, lesson }) {
     newOptions[index][key] = value;
     setOptions(newOptions);
   };
+
+  const dispatch = useDispatch();
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -28,25 +33,28 @@ function CreateQuestion({ setCreate, lesson }) {
       options: options,
     };
 
-    console.log(data);
+    dispatch(addQuestion(data));
   };
 
   return (
-    <form className="test__box create-q" onSubmit={handleSubmit}>
+    <form className='test__box create-q' onSubmit={handleSubmit}>
       <label>
         Введите вопрос
         <input
-          placeholder="В чём смысл жизни?"
-          type="text"
+          placeholder='В чём смысл жизни?'
+          type='text'
           value={question}
           onChange={(e) => setQuestion(e.target.value)}
         />
       </label>
       <label>
         Тип вопроса
-        <select value={inputType} onChange={(e) => setInputType(e.target.value)}>
-          <option value="radio">Выбрать один ответ</option>
-          <option value="checkbox">Выбрать несколько ответов</option>
+        <select
+          value={inputType}
+          onChange={(e) => setInputType(e.target.value)}
+        >
+          <option value='radio'>Выбрать один ответ</option>
+          <option value='checkbox'>Выбрать несколько ответов</option>
         </select>
       </label>
       <label>
@@ -54,30 +62,34 @@ function CreateQuestion({ setCreate, lesson }) {
         {options.map((option, index) => (
           <div key={index}>
             <input
-              type="text"
+              type='text'
               value={option.optionTitle}
-              onChange={(e) => handleOptionChange(index, "optionTitle", e.target.value)}
+              onChange={(e) =>
+                handleOptionChange(index, 'optionTitle', e.target.value)
+              }
             />
-            <label className="b-contain">
+            <label className='b-contain'>
               Правильный ответ
               <input
-                className="b-contain"
-                type="checkbox"
+                className='b-contain'
+                type='checkbox'
                 checked={option.right}
-                onChange={(e) => handleOptionChange(index, "right", e.target.checked)}
+                onChange={(e) =>
+                  handleOptionChange(index, 'right', e.target.checked)
+                }
               />
-              <div className="b-input"></div>
+              <div className='b-input'></div>
             </label>
           </div>
         ))}
-        <div className="create-q__btns-wrapper">
-          <button type="button" onClick={() => setCreate(false)}>
+        <div className='create-q__btns-wrapper'>
+          <button type='button' onClick={() => setCreate(false)}>
             Вернуться к вопросам
           </button>
-          <button type="button" onClick={addOption}>
+          <button type='button' onClick={addOption}>
             Добавить вариант ответа
           </button>
-          <button type="submit">Создать вопрос</button>
+          <button type='submit'>Создать вопрос</button>
         </div>
       </label>
     </form>
