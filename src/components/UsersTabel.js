17a6f3b59aa9da26d1aabc7705837e725fb3e21f
HP 6 <Shamoointhedark@gmail.com>
2023-03-14 09:44:15 +0500

    Скрывать таблицу учеников

--- src/components/UsersTabel.js
+++ src/components/UsersTabel.js
@@ -14,6 +14,7 @@ import yesImg from "../images/yes.png";
 import moreActionsImg from "../images/more_horiz.svg";
 
 function UsersTabel() {
+  const [tabelOpen, setTabelOpen] = useState(true);
   const [expandedRow, setExpandedRow] = useState(null);
   const dispatch = useDispatch();
 
@@ -44,54 +45,61 @@ function UsersTabel() {
     dispatch(makeAdmin(id));
   };
 
+  const handleCloseTabel = () => {
+    setTabelOpen(!tabelOpen);
+  };
+
   return (
-    <div className="users-panel">
-      {usersStatus == "loading" && <Loader auto={true} />}
-      {usersStatus !== "loading" && (
-        <>
-          <div className="users-panel__head-wrapper">
-            <div className="users-panel__head">Имя</div>
-            <div className="users-panel__head">Отдел</div>
-            <div className="users-panel__head">Роль</div>
-            <div className="users-panel__head">Допусчен</div>
-            <div className="users-panel__head">Действие</div>
-          </div>
-          <div className="users-panel__info">
-            {users.map((user) => {
-              return (
-                <Fragment key={user._id}>
-                  <div className="users-panel__column">{user.fullName}</div>
-                  <div className="users-panel__column">{user.workPosition}</div>
-                  <div className="users-panel__column">{user.role == "admin" ? "Администратор" : "Ученик"}</div>
-                  <div className="users-panel__column">
-                    {user.approved ? <img src={yesImg} alt="" /> : <img src={noImg} alt="" />}
-                  </div>
-                  <div className="users-panel__column users-panel__column--action">
-                    <img src={moreActionsImg} alt="" onClick={() => expandModal(user._id)} />
-                    {expandedRow === user._id && (
-                      <div className="actions">
-                        <button onClick={() => handleApproveUser(user._id)} className="actions__btn">
-                          Допустить
-                        </button>
-                        <button onClick={() => handleBlockUser(user._id)} className="actions__btn">
-                          Закрыть доступ
-                        </button>
-                        <button onClick={() => handleRemoveUser(user._id)} className="actions__btn">
-                          Удалить
-                        </button>
-                        <button onClick={() => handleMakeAdmin(user._id)} className="actions__btn">
-                          Сделать администратором
-                        </button>
-                      </div>
-                    )}
-                  </div>
-                </Fragment>
-              );
-            })}
-          </div>
-        </>
-      )}
-    </div>
+    <>
+      <div className="users-panel">
+        {usersStatus == "loading" && <Loader auto={true} />}
+        {usersStatus !== "loading" && (
+          <>
+            <div onClick={handleCloseTabel} className="users-panel__head-wrapper">
+              <div className="users-panel__head">Имя &#8595;</div>
+              <div className="users-panel__head">Отдел &#8595;</div>
+              <div className="users-panel__head">Роль &#8595;</div>
+              <div className="users-panel__head">Допусчен &#8595;</div>
+              <div className="users-panel__head">Действие &#8595;</div>
+            </div>
+            <div className={`users-panel__info ${tabelOpen ? "users-panel__info--hide" : ""}`}>
+              {users.map((user) => {
+                return (
+                  <Fragment key={user._id}>
+                    <div className="users-panel__column">{user.fullName}</div>
+                    <div className="users-panel__column">{user.workPosition}</div>
+                    <div className="users-panel__column">{user.role == "admin" ? "Администратор" : "Ученик"}</div>
+                    <div className="users-panel__column">
+                      {user.approved ? <img src={yesImg} alt="" /> : <img src={noImg} alt="" />}
+                    </div>
+                    <div className="users-panel__column users-panel__column--action">
+                      <img src={moreActionsImg} alt="" onClick={() => expandModal(user._id)} />
+                      {expandedRow === user._id && (
+                        <div className="actions">
+                          <button onClick={() => handleApproveUser(user._id)} className="actions__btn">
+                            Допустить
+                          </button>
+                          <button onClick={() => handleBlockUser(user._id)} className="actions__btn">
+                            Закрыть доступ
+                          </button>
+                          <button onClick={() => handleRemoveUser(user._id)} className="actions__btn">
+                            Удалить
+                          </button>
+                          <button onClick={() => handleMakeAdmin(user._id)} className="actions__btn">
+                            Сделать администратором
+                          </button>
+                        </div>
+                      )}
+                    </div>
+                  </Fragment>
+                );
+              })}
+            </div>
+          </>
+        )}
+      </div>
+      {tabelOpen && <p className="users-panel__p">Нажмите, чтобы развернуть таблицу*</p>}
+    </>
   );
 }
 
