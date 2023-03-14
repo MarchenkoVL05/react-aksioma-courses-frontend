2fbbfa1711502c069f97b083f9cbad41b1ced7f7
HP 6 <Shamoointhedark@gmail.com>
2023-03-14 12:45:44 +0500

    Пагинация на главной странице

--- src/components/LessonsList.js
+++ src/components/LessonsList.js
@@ -1,16 +1,27 @@
 import { useRef, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import ReactPaginate from "react-paginate";
 
 import LessonCard from "./LessonCard";
 import LessonLoader from "../components/LessonLoader";
 
 function LessonsList({ lessons, status, userInfo, searchError, searchStatus }) {
   const [isRemoveClicked, setIsRemoveClicked] = useState(false);
+  const [currentPage, setCurrentPage] = useState(0);
 
   const warningRef = useRef();
 
   const removeError = useSelector((state) => state.lesson.error);
   const message = useSelector((state) => state.lesson.message);
+
+  const itemsPerPage = 8;
+  const handlePageClick = (data) => {
+    const selectedPage = data.selected;
+    setCurrentPage(selectedPage);
+  };
+
+  const startIndex = currentPage * itemsPerPage;
+  const itemsToRender = lessons.slice(startIndex, startIndex + itemsPerPage);
 
   useEffect(() => {
     if (removeError && isRemoveClicked) {
@@ -40,29 +51,41 @@ function LessonsList({ lessons, status, userInfo, searchError, searchStatus }) {
           <h1 className="lessons__not-found">Такой урок не найден</h1>
         </section>
       ) : (
-        <section className="lessons">
-          <div ref={warningRef} className="warning">
-            {removeError || message}
-          </div>
-          <div className="lessons__wrapper">
-            <div className="lessons__inner">
-              {status === "loading"
-                ? [...Array(8)].map((_, index) => {
-                    return <LessonLoader key={index} />;
-                  })
-                : lessons.map((lesson) => {
-                    return (
-                      <LessonCard
-                        userInfo={userInfo}
-                        lesson={lesson}
-                        setIsRemoveClicked={setIsRemoveClicked}
-                        key={lesson._id}
-                      />
-                    );
-                  })}
+        <>
+          <section className="lessons">
+            <div ref={warningRef} className="warning">
+              {removeError || message}
             </div>
-          </div>
-        </section>
+            <div className="lessons__wrapper">
+              <div className="lessons__inner">
+                {status === "loading"
+                  ? [...Array(8)].map((_, index) => {
+                      return <LessonLoader key={index} />;
+                    })
+                  : itemsToRender.map((lesson) => {
+                      return (
+                        <LessonCard
+                          userInfo={userInfo}
+                          lesson={lesson}
+                          setIsRemoveClicked={setIsRemoveClicked}
+                          key={lesson._id}
+                        />
+                      );
+                    })}
+              </div>
+            </div>
+            <ReactPaginate
+              pageCount={Math.ceil(lessons.length / itemsPerPage)} // количество страниц
+              pageRangeDisplayed={3} // количество отображаемых страниц (слева и справа от текущей)
+              marginPagesDisplayed={1} // количество отображаемых границ
+              onPageChange={handlePageClick} // обработчик события выбора страницы
+              containerClassName={"pagination"} // класс для контейнера
+              activeClassName={"active"} // класс для активной страницы
+              previousLabel={"\u2190"}
+              nextLabel={"\u2192"}
+            />
+          </section>
+        </>
       )}
     </>
   );
